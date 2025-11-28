021dcdbd75d01adc8c1dddf04bdd213fed2ce89c
villno55 <adsotrabajos24@gmail.com>
2025-11-28 12:47:46 -0500

    add ficheros

--- Actividad-proyecto-en-clase/lib/main.dart
+++ Actividad-proyecto-en-clase/lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:tu_app_carros/screen/lista_auntos.dart';
+//import 'package:tu_app_carros/screen/lista_auntos.dart';
+import 'package:tu_app_carros/screen/cart_page.dart';
 
-void main() => runApp(const CarrosDeAndarApp());
+void main() {
+  runApp(MyApp());
+}
 
-class CarrosDeAndarApp extends StatelessWidget {
-  const CarrosDeAndarApp({super.key});
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'lista de carros',
-      theme: ThemeData(primarySwatch: Colors.orange),
-      home: const ListaCarrosScreen(),
+      home: CartPage(),
     );
   }
 }
